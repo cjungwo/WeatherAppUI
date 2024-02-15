808b33bcc309f084d8c0dfa494b6bd83843714b4
cjungwo <wjd516@gmail.com>
2024-02-15 20:46:05 +1100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by JungWoo Choi on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
